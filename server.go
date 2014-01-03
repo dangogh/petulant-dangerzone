a2a353f932280d5c7b15e3623b5f12ba1bf71bbd
Dan Kirkwood <dangogh@gmail.com>
2014-01-03 15:47:44 -0700

    checkpoint

--- server.go
+++ server.go
@@ -1,14 +1,63 @@
 package main
 
-//import "fmt"
+import "bytes"
+
+import "fmt"
 import "log"
 import "net"
 import "net/rpc"
-import "github.com/dangogh/petulant-dangerzone/mymath"
+import "errors"
+
+// Arith
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+type mathConn struct {
+	readBuf  bytes.Buffer
+	writeBuf bytes.Buffer
+	A, B     int
+	Quo, Rem int
+	closec   chan bool
+}
+
+func (c *mathConn) Read(b []byte) (int, error) {
+	return c.readBuf.Read(b)
+}
+
+func (c *mathConn) Write(b []byte) (int, error) {
+	return c.writeBuf.Write(b)
+}
+
+func (c *mathConn) Close() error {
+	select {
+	case c.closec <- true:
+	default:
+	}
+	return nil
+}
+
+func handleConnection(c *mathConn) {
+	c.Write(([]byte{"HELLO!!"})
+}
 
 func main() {
-	const msgLen = 1024
-	rpc.Register(new(mymath.Quotient))
+	rpc.Register(new(Arith))
 
 	// listen on the port
 	listen, err := net.Listen("tcp", ":1234")
@@ -16,7 +65,11 @@ func main() {
 		log.Fatal("listening...", err)
 	}
 	for {
-		go rpc.Accept(listen)
-
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
 }
