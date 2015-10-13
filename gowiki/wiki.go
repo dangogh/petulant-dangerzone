a79a22e534a3306c9993663dabd5b1562db43d02
Dan Kirkwood <dangogh@gmail.com>
2015-10-12 21:06:35 -0600

    remove getTitle

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -17,15 +17,6 @@ var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 //---------------------------------------------
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-	return m[2], nil
-}
-
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
